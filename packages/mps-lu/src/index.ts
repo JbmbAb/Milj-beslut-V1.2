2f3c0a1d69c6c1d1fd8ad1b43744316e687e8fe7
Jimmy Bruce <jimmy.bruce@vbu.se>
2026-09-17 16:48:32 +0200

    feat(authority): export temporal authority contract

--- packages/mps-lu/src/index.ts
+++ packages/mps-lu/src/index.ts
@@ -30,6 +30,7 @@ export * from "./artifacts/ProductViewerCapabilityArtifact";
 export * from "./artifacts/ViewerIdentityArtifact";
 export * from "./governance/GovernedAssessmentPersistence";
 export * from "./governance/LuSourceAuthorityEvidence";
+export * from "./governance/LuSourceAuthorityTemporalStatus";
 export * from "./governance/LuSourceAuthorityWiring";
 export * from "./viewer/ViewerKernel";
 
